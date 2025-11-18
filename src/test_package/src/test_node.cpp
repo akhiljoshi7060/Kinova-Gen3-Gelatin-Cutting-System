1614a34ad1624da751311ca2ce973e949232185c
akhiljoshi7060 <akhiljoshi255@gmail.com>
2025-11-17 23:12:59 -0800

    Remove old src folder

--- src/test_package/src/test_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <ros/ros.h>
-int main(int argc, char** argv) {
-ros::init(argc, argv, "test_node");
-    ros::NodeHandle nh;
-    ROS_INFO("Test node started");
-    ros::spin();
-    return 0;
-}
